3290de7ea5b8fe8ec306580d7098be9cd9c6f479
Daniel Capote Reynaud <danielcapotereynaud@gmail.com>
2024-04-19 11:22:58 -0300

    Atualizei o cabeçalho para ele ficar fixo

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,1 +1,14 @@
-     
+document.getElementById("hamburger").addEventListener("click", function(){
+     var nav = document.getElementById("navegation");
+     if (nav.style.display === "none") {
+       nav.style.display = "flex";
+     } else {
+       nav.style.display = "none";
+     }
+   });
+
+   function sm()
+   {
+     document.getElementById('testetxt').scrollIntoView({behavior: 'smooth'})
+   }
+   
